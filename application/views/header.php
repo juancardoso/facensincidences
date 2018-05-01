6e6a477fcc6c2cc5d29c49a5ec37604cbc46c48a
Juan Cardoso <juancardoso@outlook.com>
2018-04-30 21:10:17 -0300

    configuracao projeto e inicio tela de login

---
+++ application/views/header.php
@@ -0,0 +1,10 @@
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>"/>
+        <link href="<?= base_url('css/signin.css') ?>" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body>
