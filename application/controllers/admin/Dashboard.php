7d507e373b0c364e181ac5ca84424a3d93c00c2d
Juan Cardoso <juancardoso@outlook.com>
2018-05-05 02:44:25 -0300

    variavel faltante

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
 
 	public function index()
-	{        
-		$this->load->view('admin/dashboard');
+	{
+		$data['active'] = 'dashboard';        
+		$this->load->view('admin/dashboard',$data);
 	}
 }
